248fed8d8b2bd45e090c530cb95a14a5b28a2ed5
arvee4n <ryan@thestoutsonline.com>
2025-05-23 16:38:19 -0700

    Updated Tab Names/ added images to rdr2 shrine

--- components.js
+++ components.js
@@ -70,6 +70,9 @@ const Components = {
               <ul>
                 <summary><a href="/shrines/webfishing/webfishing.html">Webfishing</a></summary>
               </ul>
+              <ul>
+                <summary><a href="/shrines/rdr2/rdr2.html">Red Dead Redemption II</a></summary>
+              </ul>
             </details>
             <summary><a href="/sitemap/sitemap.html">Sitemap</a></summary>
           </ul>
